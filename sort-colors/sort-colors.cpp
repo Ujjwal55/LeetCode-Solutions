c0fcf09e343cf0e5eaae8e4f3d943e326d3d5594
Ujjwal55 <72218888+Ujjwal55@users.noreply.github.com>
2021-10-08 23:22:34 +0530

    Time: 4 ms (44.78%), Space: 8.3 MB (68.97%) - LeetHub

--- sort-colors/sort-colors.cpp
+++ sort-colors/sort-colors.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        vector<int> v;
         for(int i=0;i<nums.size();i++){
-            if(nums[i] == 0){
-                v.push_back(nums[i]);
-                // nums.erase(nums.begin()+i);
+            int min = i;
+            for(int j=i;j<nums.size();j++){
+                if(nums[min] > nums[j]){
+                    min = j;
+                }
             }
-        }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == 1){
-                v.push_back(nums[i]);
-                // nums.erase(nums.begin()+i);
-            }
-        }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == 2){
-                v.push_back(nums[i]);
-            }
-        }
-        for(int i=0;i<v.size();i++){
-            nums[i] = v[i];
+            swap(nums[min],nums[i]);
         }
     }
 };

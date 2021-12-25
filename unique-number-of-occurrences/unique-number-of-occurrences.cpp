43d834817a20394b4c31a1d4ec7119082a90e2f9
Ujjwal55 <agarwalujjwal698@gmail.com>
2021-12-26 01:44:41 +0530

    Time: 4 ms (60.51%), Space: 8.2 MB (61.64%) - LeetHub

---
+++ unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> m;
+        for(auto it: arr){
+            m[it]++;
+        }
+        unordered_set<int> s;
+        for(auto it: m){
+            if(s.find(it.second) != s.end())
+                return false;
+            s.insert(it.second);
+        }
+        return true;
+    }
+};

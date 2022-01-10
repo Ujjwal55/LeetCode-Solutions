be67dd884e94ac3b3b36703f7d8a4ace21f22665
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-01-10 19:39:40 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (82.01%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+int dp[46];
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 2 || n == 1){
+            return n;
+        }
+        else if(dp[n] != 0){
+            return dp[n];
+        }
+        dp[n] = climbStairs(n-1) + climbStairs(n-2);
+        return dp[n];
+    }
+};

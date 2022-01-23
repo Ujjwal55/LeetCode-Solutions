31b1823dc01dae2236d4c8ac48964007b6c2d67a
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-01-24 01:19:07 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (42.32%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -6,9 +6,20 @@ public:
         if(dp[i] != -1) return dp[i];
         return dp[i] = max(nums[i] + maxAmount(nums, i-2, dp), maxAmount(nums, i-1, dp));
     }
+    int bottomUp(vector<int> &nums, int n, vector<int> &dp){
+        dp[0] = nums[0];
+        for(int i = 1 ; i < n ; i++){
+            if(i > 1) dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+            else{
+                dp[i] = max(nums[i], dp[i-1]);
+            }
+        }
+        return dp[n-1];
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n+1, -1);
-        return maxAmount(nums, n-1, dp);
+        // return maxAmount(nums, n-1, dp);
+        return bottomUp(nums, n, dp);
     }
 };

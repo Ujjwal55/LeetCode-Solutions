2c628c7c631b942ceede96056bab39ce0f26f340
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-02-01 12:05:46 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (23.95%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -11,8 +11,29 @@ public:
         return dp[i][j] = printWays(m, n, i, j+1, dp) + printWays(m, n, i+1, j, dp);
         
     }
+    int tabulation(int m, int n){
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        for(int i = m-1 ; i >= 0 ; i--){
+            for(int j = n-1 ; j >= 0 ; j--){
+                if(i == m-1 && j == n-1) dp[i][j] = 1;
+                else{
+                    int right = 0;
+                    int down = 0;
+                    if(j < n-1){
+                        right = dp[i][j+1];
+                    }
+                    if(i < m-1){
+                        down = dp[i+1][j];
+                    }
+                    dp[i][j] = right + down;
+                }
+            }
+        }
+        return dp[0][0];
+    }
     int uniquePaths(int m, int n){
         vector<vector<int>> dp(m, vector<int> (n, -1));
-        return printWays(m, n, 0, 0, dp);
+        // return printWays(m, n, 0, 0, dp);
+        return tabulation(m, n);
     }
 };

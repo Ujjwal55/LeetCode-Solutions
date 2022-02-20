06feda92ce403cda59ee7ce2fdaa891e770f074c
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-02-20 14:48:31 +0530

    Time: 57 ms (41.17%), Space: 20.2 MB (10.07%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -8,6 +8,22 @@ public:
         fill(grid, m, n, row, col-1);
         fill(grid, m, n, row-1, col);
     }
+    void fillBFS(vector<vector<char>> &grid, int m, int n, int row, int col){
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            int curr_row = it.first;
+            int curr_col = it.second;
+            if(curr_row < 0 || curr_row > m-1 || curr_col < 0 || curr_col > n-1 || grid[curr_row][curr_col] == '0') continue;
+            grid[curr_row][curr_col] = '0';
+            q.push({curr_row, curr_col+1});
+            q.push({curr_row+1, curr_col});
+            q.push({curr_row, curr_col-1});
+            q.push({curr_row-1, curr_col});
+        }
+    }
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
@@ -16,7 +32,8 @@ public:
             for(int col = 0 ; col < n ; col++){
                 if(grid[row][col] == '1'){
                     ans+=1;
-                    fill(grid, m, n, row, col);
+                    // fill(grid, m, n, row, col);
+                    fillBFS(grid, m, n, row, col);
                 }
             }
         }

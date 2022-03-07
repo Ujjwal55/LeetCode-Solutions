57189e5d741c57a6570b62623c717048b0009ca3
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-03-08 00:22:22 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (68.55%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    void printCombination(vector<vector<int>> &ans, vector<int> &nums, vector<int> &temp, int target, int previous, int n, int i){
-        if(target == 0 || n == i){
-            if(target == 0){
-                ans.push_back(temp);
-            }
+    void combination(vector<int> &candidates, vector<int> &temp, vector<vector<int>> &ans, int target, int n, int idx){
+        if(target == 0){
+            ans.push_back(temp);
             return;
         }
-        for(int j = i ; j < n ; j++){
-            if(j > i && nums[j] == nums[j-1] ) continue;
-            if(nums[j] > target) break;
-            temp.push_back(nums[j]);
-            // previous = nums[j];
-            printCombination(ans, nums, temp, target-nums[j], previous, n, j+1);
+        
+        for(int i = idx ; i < n ; i++){
+            if(i > idx && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i] > target) break;
+            temp.push_back(candidates[i]);
+            combination(candidates, temp, ans, target-candidates[i], n, i+1);
             temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        sort(candidates.begin(), candidates.end());
         int n = candidates.size();
-        printCombination(ans, candidates, temp, target, 0, n, 0);
+        vector<int> temp;
+        vector<vector<int>> ans;
+        sort(candidates.begin(), candidates.end());
+        combination(candidates, temp, ans, target, n, 0);
         return ans;
     }
 };

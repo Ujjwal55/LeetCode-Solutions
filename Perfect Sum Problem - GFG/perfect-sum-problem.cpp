4c3ddaa666185bee6c8bb46e7cdc193329ecc410
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-03-18 18:24:54 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -18,10 +18,36 @@ class Solution{
 	    }
 	    return dp[i][sum] = countOfSubsets(arr, i-1, sum, dp)%mod; 
 	}
+	int bottomUp(int arr[], int n, int sum){
+	    vector<vector<long long>> dp(n+1, vector<long long> (sum+1, -1));
+	    dp[0][0] = 1;
+	   
+	   // for(int i = 0 ; i <= n ; i++){
+	   //     for(int j = 0 ; j <= sum ; j++){
+	   //         if(i == 0) dp[i][j] = 0;
+	   //         if(j == 0) dp[i][j] = 1;
+	   //     }
+	   // }
+	    for(int j = 1 ; j <= sum ; j++){
+	        dp[0][j] = 0;
+	    }
+	    for(int i = 1 ; i <= n ; i++){
+	        for(int j = 0 ; j <= sum ; j++){
+	            if(arr[i-1] <= j){
+	                dp[i][j] = (dp[i-1][j-arr[i-1]] + dp[i-1][j])%mod;
+	            }
+	            else{
+	                dp[i][j] = dp[i-1][j]%mod;
+	            }
+	        }
+	    }
+	    return dp[n][sum];
+	}
 	int perfectSum(int arr[], int n, int sum)
 	{
 	    vector<vector<long long>> dp(n+1, vector<long long> (sum+1, -1));
-	    return countOfSubsets(arr, n, sum, dp);
+	   // return countOfSubsets(arr, n, sum, dp);
+	   return bottomUp(arr, n, sum);
 	}
 	  
 };

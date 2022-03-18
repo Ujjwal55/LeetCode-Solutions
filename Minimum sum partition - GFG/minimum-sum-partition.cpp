e4cdd2105690e2b49d376f312a2da6a4c2a91c59
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-03-19 01:19:26 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -17,10 +17,10 @@ class Solution{
     int bottomUp(int arr[], int n){
         int sum = 0;
         for(int i = 0 ; i < n ; i++) sum+=arr[i];
-        vector<vector<bool>> dp(n+1, vector<bool>(sum+1, false));
+        vector<vector<bool>> dp(n+1, vector<bool>(sum/2+1, false));
         for(int i = 0 ; i <= n ; i++) dp[i][0] = true;
         for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= sum ; j++){
+            for(int j = 1 ; j <= sum/2 ; j++){
                 if(arr[i-1] <= j){
                     dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
                 }

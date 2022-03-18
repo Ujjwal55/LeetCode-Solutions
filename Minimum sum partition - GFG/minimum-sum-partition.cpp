93d1716402c46863d16165f06577c353a07793a5
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-03-19 01:12:16 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -14,6 +14,33 @@ class Solution{
         if(dp[n][sum1] != -1) return dp[n][sum1];
         return dp[n][sum1] = min(minSum(arr, n-1, sum1+arr[n-1], sum2, sum, dp), minSum(arr, n-1, sum1, sum2+arr[n-1], sum ,dp));
     }
+    int bottomUp(int arr[], int n){
+        int sum = 0;
+        for(int i = 0 ; i < n ; i++) sum+=arr[i];
+        vector<vector<bool>> dp(n+1, vector<bool>(sum+1, false));
+        for(int i = 0 ; i <= n ; i++){
+            for(int j = 0 ; j <= sum ; j++){
+                if(j == 0) dp[i][j] = true;
+            }
+        }
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= sum ; j++){
+                if(arr[i-1] <= j){
+                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        int mini = 100000009;
+        for(int s1 = 0 ; s1 <= sum/2 ; s1++){
+            if(dp[n][s1] == true){
+                mini = min(mini, abs(sum-s1)-s1);
+            }
+        }
+        return mini;
+    }
 	int minDifference(int arr[], int n)  { 
 	   int sum = 0;
 	   for(int i = 0 ; i < n ; i++) sum+=arr[i];
@@ -21,7 +48,8 @@ class Solution{
 	       return sum;
 	   }
 	   vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));
-	   return minSum(arr, n, 0, 0, sum, dp);
+	   //return minSum(arr, n, 0, 0, sum, dp);
+	   return bottomUp(arr, n);
 	} 
 };
 

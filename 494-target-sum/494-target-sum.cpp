10f8af8cdcc8eb61f7eb9219578e4e97cbf8bda4
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-03-21 19:37:58 +0530

    Time: 20 ms (62.03%), Space: 12.5 MB (37.10%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -9,12 +9,17 @@ public:
         if(dp.find(key) != dp.end()) return dp[key];
         return dp[key] = noOfWays(nums, target-nums[n-1], n-1, dp) + noOfWays(nums, target+nums[n-1], n-1, dp);
     }
-    int bottomUp(vector<int> &nums, int target){
+    int bottomUp(vector<int> &nums, int target, int sum){
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int> (target, -1));
-        for(int j = 0 ; j <= target ; j++) dp[0][j] = 0;
+        vector<vector<int>> dp(n+1, vector<int> (sum+1, 0));
+        for(int i = 0 ; i <= n ; i++){
+            for(int j = 0 ; j <= sum ; j++){
+               if(i == 0) dp[i][j] = 0;
+               if(j == 0) dp[i][j] = 1; 
+            }
+        }
         for(int i = 1 ; i <= n ; i++){
-            for(int j = 0 ; j <= target ; j++){
+            for(int j = 0 ; j <= sum ; j++){
                 if(nums[i-1] <= j){
                     dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
                 }
@@ -23,16 +28,17 @@ public:
                 }
             }
         }
-        return dp[n][target];
+        return dp[n][sum];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        target = abs(target);
         int n = nums.size();
         // vector<vector<int>> dp(n+1, vector<int> (total+1, -1));
         unordered_map<string, int> dp;
-        return noOfWays(nums, target, n, dp);
+        // return noOfWays(nums, target, n, dp);
         int sum = 0;
         for(int i = 0 ; i < n ; i++) sum += nums[i];
         if(sum < target || (sum+target)%2 != 0) return 0;
-        return bottomUp(nums, target);
+        return bottomUp(nums, target, (sum+target)/2);
     }
 };

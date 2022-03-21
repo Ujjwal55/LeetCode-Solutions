bf8195985c38e7eaff720f1ae64e5624a6ab8c37
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-03-21 19:27:57 +0530

    Time: 1031 ms (18.24%), Space: 89.3 MB (6.06%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
-    int noOfWays(vector<int> &nums, int target, int total, int n, unordered_map<string, int> &dp){
+    int noOfWays(vector<int> &nums, int target, int n, unordered_map<string, int> &dp){
         if(n == 0){
             if(target == 0) return 1;
             return 0;
         }
         string key = to_string(n) + " " + to_string(target);
         if(dp.find(key) != dp.end()) return dp[key];
-        return dp[key] = noOfWays(nums, target-nums[n-1], total, n-1, dp) + noOfWays(nums, target+nums[n-1], total, n-1, dp);
+        return dp[key] = noOfWays(nums, target-nums[n-1], n-1, dp) + noOfWays(nums, target+nums[n-1], n-1, dp);
+    }
+    int bottomUp(vector<int> &nums, int target){
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int> (target, -1));
+        for(int j = 0 ; j <= target ; j++) dp[0][j] = 0;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 0 ; j <= target ; j++){
+                if(nums[i-1] <= j){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][target];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        int total = 0;
-        for(int i = 0 ; i < n ; i++) total+=nums[i];
         // vector<vector<int>> dp(n+1, vector<int> (total+1, -1));
         unordered_map<string, int> dp;
-        return noOfWays(nums, target, total, n, dp);
+        return noOfWays(nums, target, n, dp);
+        int sum = 0;
+        for(int i = 0 ; i < n ; i++) sum += nums[i];
+        if(sum < target || (sum+target)%2 != 0) return 0;
+        return bottomUp(nums, target);
     }
 };

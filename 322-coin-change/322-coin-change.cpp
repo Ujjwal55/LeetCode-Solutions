d4b88383431718fec930a696c35c7975a1cd9f61
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-03-22 11:47:08 +0530

    Time: 128 ms (44.96%), Space: 87.9 MB (5.10%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -12,10 +12,33 @@ public:
             return dp[n][amount] = numberOfCoins(coins, amount, n-1, dp);
         }
     }
+    int bottomUp(vector<int> &coins, int amount){
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
+        for(int i = 0 ; i <= n ; i++){
+            for(int j = 0 ; j <= amount ; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = (i == 0) ? INT_MAX-1 : 0;
+                }
+            }
+        }
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= amount ; j++){
+                if(coins[i-1] <= j){
+                    dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
-        int minCoins = numberOfCoins(coins, amount, n, dp);
+        // int minCoins = numberOfCoins(coins, amount, n, dp);
+        int minCoins = bottomUp(coins, amount);
         return minCoins == INT_MAX-1 ? -1 : minCoins;
     }
 };

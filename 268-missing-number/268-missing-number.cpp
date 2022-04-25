6799c3c9687d10581ff9c255cfec048f80d0473d
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-04-25 19:25:20 +0530

    Time: 16 ms (91.80%), Space: 17.9 MB (65.65%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -10,10 +10,27 @@ public:
 //         }
         
 //         return ans;
-        int n = nums.size();
-        int sum = n * (n + 1) / 2;
-        for(int i = 0 ; i < n ; i++) sum -= nums[i];
+//         int n = nums.size();
+//         int sum = n * (n + 1) / 2;
+//         for(int i = 0 ; i < n ; i++) sum -= nums[i];
         
-        return sum;
+//         return sum;
+        
+        int n = nums.size();
+        int i = 0;
+        
+        while(i < n){
+            if(nums[i] == n) i++;
+            else if(nums[i] != i){
+                swap(nums[i], nums[nums[i]]);
+            }
+            else i++;
+        }
+        
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] != i) return i;
+        }
+        
+        return n;
     }
 };

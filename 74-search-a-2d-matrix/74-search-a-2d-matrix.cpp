d8e14c13d34a10a9252e8e83d37e0517cb430900
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-04-26 19:36:17 +0530

    Time: 6 ms (48.57%), Space: 9.6 MB (53.45%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -21,28 +21,41 @@ public:
 //         return false;
 //     }
     
+//     bool searchMatrix(vector<vector<int>> &matrix, int target){
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+        
+//         int top = 0, bottom = m - 1, row = -1;
+//         while(top <= bottom){
+//             int mid = (top + bottom) / 2;
+//             if(target > matrix[mid][n - 1]) top = mid + 1;
+//             else if(target < matrix[mid][0]) bottom = mid - 1;
+//             else{
+//                 row = mid;
+//                 break;
+//             }
+//         }
+        
+//         if(row == -1) return false;
+//         top = 0, bottom = n - 1;
+//         while(top <= bottom){
+//             int mid = (top + bottom) / 2;
+//             if(target == matrix[row][mid]) return true;
+//             if(target > matrix[row][mid]) top = mid + 1;
+//             else if(target < matrix[row][mid]) bottom = mid - 1;
+//         }
+//         return false;
+//     }
+    
     bool searchMatrix(vector<vector<int>> &matrix, int target){
         int m = matrix.size();
         int n = matrix[0].size();
         
-        int top = 0, bottom = m - 1, row = -1;
-        while(top <= bottom){
-            int mid = (top + bottom) / 2;
-            if(target > matrix[mid][n - 1]) top = mid + 1;
-            else if(target < matrix[mid][0]) bottom = mid - 1;
-            else{
-                row = mid;
-                break;
-            }
-        }
-        
-        if(row == -1) return false;
-        top = 0, bottom = n - 1;
-        while(top <= bottom){
-            int mid = (top + bottom) / 2;
-            if(target == matrix[row][mid]) return true;
-            if(target > matrix[row][mid]) top = mid + 1;
-            else if(target < matrix[row][mid]) bottom = mid - 1;
+        int i = 0, j = n-1;
+        while(i < m && j >= 0){
+            if(target == matrix[i][j]) return true;
+            else if(target > matrix[i][j]) i++;
+            else j--;
         }
         return false;
     }

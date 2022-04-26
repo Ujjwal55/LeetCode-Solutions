806875578d4b7e9a389d57cd18406e86b59f54c2
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-04-27 00:25:03 +0530

    Time: 15 ms (68.40%), Space: 15.8 MB (52.98%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -14,13 +14,41 @@ public:
 //         }
 //         return ans;
         
-        vector<int> ans;
+        // vector<int> ans;
+        // int n = nums.size();
+        // unordered_map<int, int> m;
+        // for(int i = 0 ; i < n ; i++) m[nums[i]]++;
+        // for(auto it: m){
+        //     if(it.second > n/3) ans.push_back(it.first);
+        // }
+        // return ans;
+        
         int n = nums.size();
-        unordered_map<int, int> m;
-        for(int i = 0 ; i < n ; i++) m[nums[i]]++;
-        for(auto it: m){
-            if(it.second > n/3) ans.push_back(it.first);
+        vector<int> ans;
+        int num1 = INT_MAX, num2 = INT_MAX;
+        int cnt1 = 0, cnt2 = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(num1 == nums[i]) cnt1++;
+            else if(num2 == nums[i]) cnt2++;
+            else if(cnt1 == 0){
+                num1 = nums[i];
+                cnt1++;
+            }
+            else if(cnt2 == 0){
+                num2 = nums[i];
+                cnt2++;
+            }
+            else{
+                cnt1--, cnt2--;
+            }
         }
+        cnt1 = 0, cnt2 = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] == num1) cnt1++;
+            else if(nums[i] == num2) cnt2++;
+        }
+        if(cnt1 > n/3) ans.push_back(num1);
+        if(cnt2 > n/3) ans.push_back(num2);
         return ans;
     }
 };

72569b4885ff0fa74ace5006648db1d76ac65733
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-04-26 20:36:56 +0530

    Time: 49 ms (5.24%), Space: 19.4 MB (94.20%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        int num = nums[0], cnt = 1;
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         int num = nums[0], cnt = 1;
         
-        for(int i = 0 ; i < n ; i++){
-            if(num == nums[i]) cnt++;
-            else cnt--;
-            if(cnt == 0){
-                num = nums[i];
-                cnt++;
-            }
-        }
-        
-//         cnt = 0;
 //         for(int i = 0 ; i < n ; i++){
-//             if(nums[i] == num) cnt++;
+//             if(num == nums[i]) cnt++;
+//             else cnt--;
+//             if(cnt == 0){
+//                 num = nums[i];
+//                 cnt++;
+//             }
 //         }
         
-        return num;
+// //         cnt = 0;
+// //         for(int i = 0 ; i < n ; i++){
+// //             if(nums[i] == num) cnt++;
+// //         }
         
+//         return num;
+        
+//     }
+    
+    int majorityElement(vector<int> &nums){
+        int n = nums.size();
+        int candidate = 0;
+        
+        for(int i = 0 ; i < 32 ; i++){
+            int ones = 0;
+            for(int j = 0 ; j < n ; j++){
+                if(nums[j] & (1 << i)) ones++;
+            }
+            if(ones > n/2) candidate += (1 << i);
+        }
+        
+        return candidate;
     }
 };

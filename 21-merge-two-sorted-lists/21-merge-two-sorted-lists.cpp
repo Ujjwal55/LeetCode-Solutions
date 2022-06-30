e1ec3d03bff402c27b2468b31d534e7e799342e6
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-06-30 23:25:20 +0530

    Time: 8 ms (78.89%), Space: 14.8 MB (42.40%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,28 +11,39 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        // if(!l1) return l2;
+        // if(!l2) return l1;
+        // ListNode *temp1 = l1, *temp2 = l2;
+        // ListNode *a = NULL;
+        // ListNode *prev = NULL;
+        // while(l1 && l2){
+        //     if(l1->val <= l2->val){
+        //         while(l1 && l1->val <= l2->val){
+        //             prev = l1;
+        //             l1 = l1->next;
+        //         }
+        //         prev->next = l2;
+        //     }
+        //     else{
+        //         while(l2 && l2->val < l1->val){
+        //             prev = l2;
+        //             l2 = l2->next;
+        //         }
+        //         prev->next = l1;
+        //     }
+        // }
+        // if(temp1->val <= temp2->val) return temp1;
+        // else return temp2;
         if(!l1) return l2;
         if(!l2) return l1;
-        ListNode *temp1 = l1, *temp2 = l2;
-        ListNode *a = NULL;
-        ListNode *prev = NULL;
-        while(l1 && l2){
-            if(l1->val <= l2->val){
-                while(l1 && l1->val <= l2->val){
-                    prev = l1;
-                    l1 = l1->next;
-                }
-                prev->next = l2;
-            }
-            else{
-                while(l2 && l2->val < l1->val){
-                    prev = l2;
-                    l2 = l2->next;
-                }
-                prev->next = l1;
-            }
+        
+        if(l1->val <= l2->val){
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
         }
-        if(temp1->val <= temp2->val) return temp1;
-        else return temp2;
+        else{
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
     }
 };

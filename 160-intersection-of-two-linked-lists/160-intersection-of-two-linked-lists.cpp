ec7e10bc822dc886bf1d55bf810fc8da4406ecf0
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-07-02 00:43:17 +0530

    Time: 29 ms (100.00%), Space: 14.6 MB (31.44%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,37 +9,12 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int n = 0, m = 0;
+        if(headA == NULL || headB == NULL) return 0;
         ListNode *temp1 = headA, *temp2 = headB;
-        while(temp1){
-            n++;
-            temp1 = temp1->next;
+        while(temp1 != temp2){
+            temp1 = temp1 == NULL ? headB : temp1->next;
+            temp2 = temp2 == NULL ? headA : temp2->next;
         }
-        while(temp2){
-            m++;
-            temp2 = temp2->next;
-        }
-        int diff = abs(m-n);
-        temp1 = headA, temp2 = headB;
-        if(n > m){
-            int i = 0;
-            while(i < diff){
-                temp1 = temp1->next;
-                i++;
-            }
-        }
-        else{
-            int i = 0;
-            while(i < diff){
-                temp2 = temp2->next;
-                i++;
-            }
-        }
-        while(temp1 && temp2){
-            if(temp1 == temp2) return temp1;
-            temp1 = temp1->next;
-            temp2 = temp2->next;
-        }
-        return 0;
+        return temp1;
     }
 };

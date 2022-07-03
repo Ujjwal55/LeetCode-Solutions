7b70f1adcaef2da51a55dea9a5d28d7cb69775a0
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-07-03 18:45:36 +0530

    Time: 7 ms (56.76%), Space: 10.6 MB (74.53%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -10,27 +10,37 @@
  */
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(!head || !head->next) return NULL;
-        // ListNode *a = new ListNode(-1);
-        // a->next = head;
-        ListNode *prev = NULL;
-        ListNode *curr = head;
-        int i = 1;
-        ListNode *temp = head;
-        // head = a;
-        while(temp){
-            if(i > n){
-                prev = curr;
-                curr = curr->next;
-            }
-            i++;
-            temp = temp->next;
+    int remove(ListNode *head, int n){
+        if(!head->next) return 0;
+        int cnt = remove(head->next, n) + 1;
+        if(cnt == n){
+            head->next = head->next->next;
         }
-        if(prev == NULL) return head->next;
-        ListNode *p = prev->next;
-        prev->next = prev->next->next;
-        delete p;
-        return head;
+        return cnt;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // if(!head || !head->next) return NULL;
+        // // ListNode *a = new ListNode(-1);
+        // // a->next = head;
+        // ListNode *prev = NULL;
+        // ListNode *curr = head;
+        // int i = 1;
+        // ListNode *temp = head;
+        // // head = a;
+        // while(temp){
+        //     if(i > n){
+        //         prev = curr;
+        //         curr = curr->next;
+        //     }
+        //     i++;
+        //     temp = temp->next;
+        // }
+        // if(prev == NULL) return head->next;
+        // ListNode *p = prev->next;
+        // prev->next = prev->next->next;
+        // delete p;
+        // return head;
+        int num = remove(head, n);
+        return num < n ? head->next : head;
     }
 };

a3887ef07c8d14252e1c73ad5639a590ddb95b80
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-07-04 11:33:59 +0530

    Time: 54 ms (51.79%), Space: 71.6 MB (11.54%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -10,27 +10,47 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *s = new ListNode(-1);
-        ListNode *temp = s;
-        int carry = 0;
-        int sum;
-        while(l1 || l2 || carry){
-            sum = 0;
-            if(l1){
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if(l2){
-                sum += l2->val;
-                l2 = l2->next;
-            }
-            sum += carry;
-            carry = sum/10;
-            ListNode *a = new ListNode(sum%10);
-            s->next = a;
-            s = a;
+    ListNode *recurse(ListNode *l1, ListNode *l2, int carry){
+        if(!l1 && !l2){
+            if(carry) return new ListNode(1);
+            return NULL;
         }
-        return temp->next;
+        int sum = 0;
+        if(l1){
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if(l2){
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        sum += carry;
+        ListNode *s = new ListNode(sum%10);
+        s->next = recurse(l1, l2, sum/10);
+        return s;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // ListNode *s = new ListNode(-1);
+        // ListNode *temp = s;
+        // int carry = 0;
+        // int sum;
+        // while(l1 || l2 || carry){
+        //     sum = 0;
+        //     if(l1){
+        //         sum += l1->val;
+        //         l1 = l1->next;
+        //     }
+        //     if(l2){
+        //         sum += l2->val;
+        //         l2 = l2->next;
+        //     }
+        //     sum += carry;
+        //     carry = sum/10;
+        //     ListNode *a = new ListNode(sum%10);
+        //     s->next = a;
+        //     s = a;
+        // }
+        // return temp->next;
+        return recurse(l1, l2, 0);
     }
 };

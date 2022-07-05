9fe4ba4db9c0c16681aeaee4dd2ac6fbf4620346
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-07-05 12:11:37 +0530

    Time: 21 ms (14.12%), Space: 11.3 MB (59.86%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,19 +17,45 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node *, Node *> m;
-        Node *curr = head;
-        while(curr){
-            Node *temp = new Node(curr->val);
-            m[curr] = temp;
-            curr = curr->next;
+        // unordered_map<Node *, Node *> m;
+        // Node *curr = head;
+        // while(curr){
+        //     Node *temp = new Node(curr->val);
+        //     m[curr] = temp;
+        //     curr = curr->next;
+        // }
+        // curr = head;
+        // while(curr){
+        //     m[curr]->next = m[curr->next];
+        //     m[curr]->random = m[curr->random];
+        //     curr = curr->next;
+        // }
+        // return m[head];
+        if(!head) return head;
+        Node *temp = head;
+        while(temp){
+            Node *curr = new Node(temp->val);
+            curr->next = temp->next;
+            temp->next = curr;
+            temp = temp->next->next;
         }
-        curr = head;
-        while(curr){
-            m[curr]->next = m[curr->next];
-            m[curr]->random = m[curr->random];
-            curr = curr->next;
+        temp = head;
+        while(temp){
+            if(temp->random){
+                temp->next->random = temp->random->next;
+            }
+            temp = temp->next->next;
         }
-        return m[head];
+        Node *clone = head->next , *temp1 = head->next;
+        temp = head;
+        while(temp){
+            temp->next = temp1->next;
+            if(temp1->next){
+                temp1->next = temp1->next->next;
+            }
+            temp = temp->next;
+            temp1 = temp1->next;
+        }
+        return clone;
     }
 };

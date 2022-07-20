4132886f568aec7b121570d2213fc5d88c3e5fde
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-07-20 23:08:21 +0530

    Time: 1589 ms (5.01%), Space: 19.9 MB (12.23%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    void permutations(vector<int> &nums, vector<vector<int>> &ans, vector<int> &temp, vector<bool> &visited, int n){
+    void permutation(vector<vector<int>> &ans, vector<int> &temp, vector<int> &nums, vector<bool> &visited, int n){
         if(temp.size() == n){
             vector<int> t = temp;
-            if(find(ans.begin(), ans.end(), t) == ans.end()) ans.push_back(temp);
+            if(find(ans.begin(), ans.end(), t) == ans.end()){
+                ans.push_back(temp);
+            }
             return;
         }
-        for(int i = 0 ; i < n ; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                temp.push_back(nums[i]);
-                permutations(nums, ans, temp, visited, n);
-                visited[i] = false;
+        for(int idx = 0 ; idx < n ; idx++){
+            if(!visited[idx]){
+                visited[idx] = true;
+                temp.push_back(nums[idx]);
+                permutation(ans, temp, nums, visited, n);
+                visited[idx] = false;
                 temp.pop_back();
             }
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        int n = nums.size();
-        vector<bool> visited(n, false);
         vector<vector<int>> ans;
         vector<int> temp;
-        permutations(nums, ans, temp, visited, n);
+        int n = nums.size();
+        vector<bool> visited(n, false);
+        permutation(ans, temp, nums, visited, n);
         return ans;
     }
 };

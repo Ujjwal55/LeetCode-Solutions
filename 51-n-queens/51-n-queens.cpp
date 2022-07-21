e1ae10f34d81de91821a4d83ecec8008f69e3412
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-07-21 23:07:34 +0530

    Time: 11 ms (54.57%), Space: 7.2 MB (70.91%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool isValid(vector<string> &s, int n, int row, int col){
-        for(int i = row ; i >=0 ; i--){
+        for(int i = row ; i >= 0 ; i--){
             if(s[i][col] == 'Q') return false;
         }
-        for(int i = row, j = col ; i >= 0 && col < n ; i-- , j++){
+        for(int i = row, j = col ; i >= 0 && j >= 0 ; i--, j--){
             if(s[i][j] == 'Q') return false;
         }
-        for(int i = row, j = col ; i >= 0 && j>=0 ; i--,j--){
+        for(int i = row, j = col ; i >= 0 && j < n ; i--, j++){
             if(s[i][j] == 'Q') return false;
         }
         return true;
     }
-    void printQueens(int n, vector<vector<string>> &ans, vector<string> &s, int i, int j){
+    void printQueens(vector<vector<string>> &ans, vector<string> &s, int n, int i, int j){
         if(i == n){
             ans.push_back(s);
             return;
@@ -20,16 +20,15 @@ public:
         for(int j = 0 ; j < n ; j++){
             if(isValid(s, n, i, j)){
                 s[i][j] = 'Q';
-                printQueens(n, ans, s, i+1, j);
+                printQueens(ans, s, n, i+1, j);
                 s[i][j] = '.';
             }
         }
-        
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> s(n, string(n,'.'));
-        printQueens(n, ans, s, 0, 0);
+        vector<string> s(n, string(n, '.'));
+        printQueens(ans, s, n, 0, 0);
         return ans;
     }
 };

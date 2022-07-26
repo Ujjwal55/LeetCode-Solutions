9109fd80bd9593a57270e5b6ea56d1e8e015b08d
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-07-26 18:23:48 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (26.69%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int decode(string &s, int i, int n, vector<int> &dp){
-        if(i >= n) return 1;
+    int decode(string &s, int i, vector<int> &dp){
+        if(i < 0) return 1;
         if(dp[i] != -1) return dp[i];
+        int ans = 0, op2 = 0;
         int op1 = s[i] - '0';
-        int op2 = 0, ans = 0;
-        if(i < n-1){
-            op2 = op1*10 + s[i+1] - '0';
+        if(i > 0){
+            op2 = (s[i-1]-'0')*10 + op1;
         }
-        if(op1 > 0) ans += decode(s, i+1, n, dp);
-        if(op2 >= 10 && op2 <= 26) ans += decode(s, i+2, n, dp);
+        if(op1 > 0) ans += decode(s, i-1, dp);
+        if(op2 >= 10 && op2 <= 26) ans += decode(s, i-2, dp);
         return dp[i] = ans;
     }
     int numDecodings(string s) {
-        int n = s.length();
+        int n = s.size();
         vector<int> dp(n+1, -1);
-        int ans = decode(s, 0, n ,dp);
+        int ans = decode(s, n-1, dp);
         return ans;
     }
 };

82530c68c6fefafd6d70d3c5c7289cebb2cb7fb2
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-08-08 18:45:01 +0530

    Time: 185 ms (31.22%), Space: 50.2 MB (9.40%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,44 +1,20 @@
 class Solution {
 public:
-    int numberOfCoins(vector<int> &coins, int amount, int n, vector<vector<int>> &dp){
-        if(n == 0 || amount == 0){
-            return (amount == 0) ? 0 : INT_MAX-1;
-        }
-        if(dp[n][amount] != -1) return dp[n][amount];
-        if(coins[n-1] <= amount){
-            return dp[n][amount] = min(1 + numberOfCoins(coins, amount-coins[n-1], n, dp), numberOfCoins(coins, amount, n-1, dp));
+    int minCoins(vector<int> &coins, int sz, int amount, vector<vector<int>> &minimumCoins){
+        if(amount == 0) return 0;
+        if(sz == 0) return INT_MAX-1;
+        if(minimumCoins[sz][amount] != -1) return minimumCoins[sz][amount];
+        if(coins[sz-1] <= amount){
+            return minimumCoins[sz][amount] = min(1 + minCoins(coins, sz, amount-coins[sz-1], minimumCoins), minCoins(coins, sz-1, amount, minimumCoins));
         }
         else{
-            return dp[n][amount] = numberOfCoins(coins, amount, n-1, dp);
+            return minimumCoins[sz][amount] = minCoins(coins, sz-1, amount, minimumCoins);
         }
-    }
-    int bottomUp(vector<int> &coins, int amount){
-        int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
-        for(int i = 0 ; i <= n ; i++){
-            for(int j = 0 ; j <= amount ; j++){
-                if(i == 0 || j == 0){
-                    dp[i][j] = (i == 0) ? INT_MAX-1 : 0;
-                }
-            }
-        }
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= amount ; j++){
-                if(coins[i-1] <= j){
-                    dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][amount];
     }
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
-        // int minCoins = numberOfCoins(coins, amount, n, dp);
-        int minCoins = bottomUp(coins, amount);
-        return minCoins == INT_MAX-1 ? -1 : minCoins;
+        int sz = coins.size();
+        vector<vector<int>> minimumCoins(sz+1, vector<int> (amount+1, -1));
+        int ans = minCoins(coins, sz, amount, minimumCoins);
+        return ans == INT_MAX-1 ? -1 : ans;
     }
 };

11a4fee9973c42ff000b6f44bf26aa5148e198a4
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-08-23 23:40:48 +0530

    Time: 414 ms (37.32%), Space: 117.9 MB (59.47%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,37 +10,30 @@
  */
 class Solution {
 public:
-    bool check(ListNode *&head, ListNode* temp){
-        if(!temp) return true;
-        bool isPal = check(head, temp->next);
-        if(temp->val != head->val) return false;
-        head = head->next;
-        return isPal;
-    }
     bool isPalindrome(ListNode* head) {
-        // ListNode *slow = head, *fast = head;
-        // while(fast && fast->next && fast->next->next){
-        //     slow = slow->next;
-        //     fast = fast->next->next;
-        // }
-        // ListNode *a = slow->next;
-        // ListNode *curr = NULL;
-        // while(a){
-        //     ListNode *temp = a->next;
-        //     a->next = curr;
-        //     curr = a;
-        //     a = temp;
-        // }
-        // slow->next = curr;
-        // slow = head;
-        // while(curr){
-        //     if(curr->val != slow->val) return false;
-        //     curr = curr->next;
-        //     slow = slow->next;
-        // }
-        // return true;
         
-        return check(head, head);
-        
+        ListNode *slow = head, *fast = head;
+        ListNode *prev;
+        while(fast && fast->next && fast->next->next){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode *a = slow->next;
+        ListNode *curr = NULL;
+        while(a){
+            ListNode *temp = a->next;
+            a->next = curr;
+            curr = a;
+            a = temp;
+        }
+        slow->next = curr;
+        slow = head;
+        while(curr){
+            if(curr->val != slow->val) return false;
+            curr = curr->next;
+            slow = slow->next;
+        }
+        return true;
     }
 };

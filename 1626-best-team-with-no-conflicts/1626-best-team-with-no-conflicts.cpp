72366764f18e7f125b458a79ef306efdee9db434
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-09-06 10:19:40 +0530

    Time: 1137 ms (5.08%), Space: 386.2 MB (5.43%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -4,12 +4,12 @@ public:
         if(i >= temp.size()){
             return 0;
         }
-        if(dp[i][maxAge] != -1) return dp[i][maxAge];
+        if(dp[maxAge][i] != -1) return dp[maxAge][i];
         if(temp[i][1] >= maxAge){
-            return dp[i][maxAge] = max(temp[i][0] + bestScore(temp, i+1, temp[i][1], dp), bestScore(temp, i+1, maxAge, dp));
+            return dp[maxAge][i] = max(temp[i][0] + bestScore(temp, i+1, temp[i][1], dp), bestScore(temp, i+1, maxAge, dp));
         }
         else{
-            return dp[i][maxAge] = bestScore(temp, i+1, maxAge, dp);
+            return dp[maxAge][i] = bestScore(temp, i+1, maxAge, dp);
         }
     }
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {

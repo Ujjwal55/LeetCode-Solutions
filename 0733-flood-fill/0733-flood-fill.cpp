ab7d563fbca8a855ae5eb809d613b55e5c69ee2a
Ujjwal55 <agarwalujjwal698@gmail.com>
2022-11-12 00:54:14 +0530

    Time: 9 ms (91.48%), Space: 14 MB (48.44%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -11,13 +11,28 @@ public:
         fill(image, sr, sc-1, startingColor, color, m, n);
         return;
     }
-
+    void fillBFS(vector<vector<int>> &image, int sr, int sc, int startingColor, int color, int m, int n){
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            int currRow = it.first;
+            int currCol = it.second;
+            if(currRow < 0 || currRow > m-1 || currCol < 0 || currCol > n-1 || image[currRow][currCol] != startingColor) continue;
+            image[currRow][currCol] = color;
+            q.push({currRow+1, currCol});
+            q.push({currRow, currCol+1});
+            q.push({currRow-1, currCol});
+            q.push({currRow, currCol-1});  
+        }
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m = image.size();
         int n = image[0].size();
         int startingColor = image[sr][sc];
         if(startingColor == color) return image;
-        fill(image, sr, sc, startingColor, color, m, n);
+        fillBFS(image, sr, sc, startingColor, color, m, n);
         return image;
     }
 };
